ef1ce00ec958bb55f1a6f99d19619b75b6cc1770
Your Name <1@a.com>
2021-07-22 12:44:15 +0300

    Make dnsmasq work on 53

--- stubby/entrypoint.sh
+++ stubby/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-/opt/stubby/bin/stubby -g -C /opt/stubby/etc/stubby/stubby.yml
 
-dnsmasq -d -q -C /etc/my-dnsmasq.conf
+dnsmasq -C /etc/my-dnsmasq.conf
+
+/opt/stubby/bin/stubby -C /opt/stubby/etc/stubby/stubby.yml
